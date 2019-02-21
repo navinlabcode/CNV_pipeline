3b6d2407121e49b40d5e9678ba59a1b336f780d9
MinBio <min.hu.bioinfo@gmail.com>
2019-02-21 12:01:40 -0600

    Add files via upload

---
+++ run_CNA.sh
@@ -0,0 +1,200 @@
+#!/bin/sh
+
+#Initial Processing for identifying the existence of Copy number variations (CNVs) 
+#Depends on: Bowtie, Samtools, GNU Parallel, R package(DNAcopy).
+#Author: Min Hu
+#Last Updated: Nov 13, 2018
+
+
+main(){
+	create_folder
+	run_bowtie_varbin
+	read_align_stat
+	filter
+	segmentation
+	heatmap
+     }
+
+help="
+***version 1.3****
+Name 
+        run_CNV.sh -- Call CNV in a population of N samples
+
+Description
+
+        Initial Processing for identifying the existence of Copy number variations (CNVs)
+        
+Usage
+        sh `basename $0` [--fastq] <fastq_path_file> [--output] <outdir_path> [--sample] <tissue sample name> [--res] <resolution of bin size> [--cpu] < Cpu number> [--undo_prune] <the proportional increase number for the CBS segment function> [alpha] <significance levels for the CBS test [filter_CellWithEmptyBin] < filter the cells with empty reads in bins > [--makeFig] <plot the CNV profile per cell>  1>process.info 2>process.log 
+
+arguments:
+        -f|--fastq: [ The path file to the fastq ] 
+	-o|--output: [ output(default) ]
+	-s|--sample: [ sample(default) ]
+	-r|--res:	[ 100 200(default) 300 400 500 1000 2000 5000 10000 ]
+	-c|--cpu: [ 30(default) ]
+	-u|--undo_prune: [ 0.05(default) ]
+	-a|--alpha: [ 0.0001(default) ]
+	-e|--filter_CellWithEmptyBin: [ 0.1(default) ]
+	-m|--makeFig: [ TRUE(default) NO]
+Example:
+	sh `basename $0` --fastq fastq_input --output output --sample Tn5
+or 
+	sh `basename $0` --fastq fastq_input --output output --sample Tn5 --res 200 --cpu 30 --undo_prune 0.05 --alpha 0.0001 --makeFig "TRUE"
+"
+
+if [[ $# -lt 3 ]] ; then echo "$help" ; exit 1 ; fi
+
+TEMP=`getopt -o f:o:s:r:c:u:a:e:m: --long fastq:,output:,sample:,res:,cpu:,undo_prune:,alpha:,filter_CellWithEmptyBin:,makeFig: \
+        -n 'example.bash' -- "$@"`
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+eval set -- "$TEMP"
+while true ; do
+        case "$1" in
+        -f|--fastq)
+                fastq_input=$2 ; shift 2 ;;
+        -o|--output)
+                output=$2 ; shift 2 ;;
+        -s|--sample)
+                sample=$2 ; shift 2 ;;
+        -r|--res)
+                res=$2 ; shift 2 ;;
+        -c|--cpu)
+                cpu=$2 ; shift 2 ;;
+        -u|--undo_prune)
+             	undo_prune=$2 ; shift 2 ;;
+        -a|--alpha)
+                alpha=$2 ; shift 2 ;;
+        -e|--filter_CellWithEmptyBin)
+                filter_CellWithEmptyBin=$2 ; shift 2 ;;
+	-m|--makeFig)
+		makeFig=$2 ; shift 2 ;;
+	--) shift ; break ;;
+	*) echo "Internal error!" ; exit 1 ;;
+	esac
+done
+
+[ -z "$output" ] && output="output"
+[ -z "$sample" ] && sample="sample"
+[ -z "$res" ] && res="200"
+[ -z "$cpu" ] && cpu="30"
+[ -z "$undo_prune" ] && undo_prune="0.05"
+[ -z "$alpha" ] && alpha="0.0001"
+[ -z "$makeFig" ] && makeFig="TRUE"
+[ -z "$filter_CellWithEmptyBin" ] && filter_CellWithEmptyBin="0.1"
+
+create_folder(){
+	bin=`dirname $0`/bin
+	lib=`dirname $0`/lib
+	sam_folder="$output/sam"
+	bam_folder="$output/bam"
+	sort_folder="$output/sort"
+	vbdir_folder="$output/vbdir"
+	stat_folder="$output/stat"
+	metrics_folder="$output/metrics"
+	logs_folder="$output/logs"
+	final_folder="$output/final_result"
+	mkdir -p $sam_folder/
+	mkdir -p $bam_folder/
+	mkdir -p $sort_folder/
+	mkdir -p $vbdir_folder/
+	mkdir -p $metrics_folder/
+	mkdir -p $stat_folder/
+	mkdir -p $logs_folder/
+	mkdir -p $final_folder/
+}
+
+run_bowtie_varbin(){
+	[ -e "$output/split_bowtie.sh" ] && `rm -f "$output/split_bowtie.sh"`
+	s1_tmp="$output/split_bowtie.sh"
+	s2_tmp="$output/split_bowtie_uniq.sh"	
+	cat $fastq_input |grep -v "R2" | while read line
+	do
+	s1_input=${line}
+	s2_input=`ls $s1_input|sed -e 's/_R1_/_R2_/g'`	
+	s3_input=`ls $s1_input|sed -e 's/L00[0-9]/L003/g'`
+	if [ -f $s1_input ] && [ ! -f $s3_input ]
+	then 
+		if [ ! -f $s2_input ] || [ $s1_input = $s2_input ]
+		then
+		s2_input="";
+		fi
+		echo "perl $bin/run_bowtie2_varbin.pl -fq1 $s1_input -fq2 $s2_input -samdir $sam_folder -bamdir $bam_folder -sortdir $sort_folder -vb_dir $vbdir_folder -stat_dir $stat_folder -res $res" >> $s1_tmp
+	else
+	s1_dir=`dirname $s1_input`	
+	echo "perl $bin/run_bowtie2_varbin_nextseq.pl -fqdir $s1_dir -samdir $sam_folder -bamdir $bam_folder -sortdir $sort_folder -vb_dir $vbdir_folder -stat_dir $stat_folder -res $res " >> $s1_tmp 
+	
+	fi
+	
+	done
+	sort $s1_tmp |uniq > $s2_tmp
+	gnu_parallel=$(grep "parallel" $lib/CNA.config | cut -d "=" -f 2)
+	$gnu_parallel -j $cpu < $s2_tmp 
+	rm -f $output/bowtie-*
+	rm -f $s1_tmp
+	rm -f $s2_tmp
+	time=`date`
+	echo "$time step1 run_bowtie & varbin  is done"
+}
+
+
+read_align_stat(){
+        perl $bin/All_stat_metrics.pl $stat_folder > "$metrics_folder/all_stat_metrics.txt"
+        perl $bin/All_stat_metrics_summary.pl $stat_folder > "$metrics_folder/all_stat_metrics_summary.txt"
+        perl $bin/All_raw_bin_metrics.pl $vbdir_folder > "$metrics_folder/all_raw_bins_metrics.txt"
+}
+
+
+
+filter(){
+if [[ $res == "100" ]];then
+        filterFile=$lib/$(grep "excluded_bin_100k" $lib/CNA.config | cut -d "=" -f 2 |xargs basename)
+        gcinputFile=$lib/$(grep "gc_100k" $lib/CNA.config | cut -d "=" -f 2 |xargs basename)
+elif [[ $res == "200" ]];then
+        filterFile=$lib/$(grep "excluded_bin_200k" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+        gcinputFile=$lib/$(grep "gc_200k" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+elif [[ $res == "300" ]];then
+        filterFile=$lib/$(grep "excluded_bin_300k" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+        gcinputFile=$lib/$(grep "gc_300k" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+elif [[ $res == "400" ]];then
+        filterFile=$lib/$(grep "excluded_bin_400k" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+        gcinputFile=$lib/$(grep "gc_400k" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+elif [[ $res == "500" ]];then
+        filterFile=$lib/$(grep "excluded_bin_500k" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+        gcinputFile=$lib/$(grep "gc_500k" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+elif [[ $res == "1000"  ]];then
+        filterFile=$lib/$(grep "excluded_bin_1M" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+        gcinputFile=$lib/$(grep "gc_1M" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+ elif [[ $res == "2000"  ]];then
+        filterFile=$lib/$(grep "excluded_bin_2M" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+        gcinputFile=$lib/$(grep "gc_2M" $lib/CNA.config | cut -d "=" -f 2b|xargs basename)
+
+ elif [[ $res == "5000"  ]];then
+        filterFile=$lib/$(grep "excluded_bin_5M" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+        gcinputFile=$lib/$(grep "gc_5M" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+ else
+        filterFile=$lib/$(grep "excluded_bin_10M" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+        gcinputFile=$lib/$(grep "gc_10M" $lib/CNA.config | cut -d "=" -f 2|xargs basename)
+ fi
+        R CMD BATCH --vanilla --args -filter="$filterFile" -gcinputFile="$gcinputFile"  -outdir="$output" -filter_CellWithEmptyBin="$filter_CellWithEmptyBin" "$bin/filterScript_GC.R" "$logs_folder/filter_log_file.log"
+}
+
+segmentation(){
+        source1=$bin/$(grep "CNProcessingCentromeres" $lib/CNA.config |cut -d "=" -f 2|xargs basename)
+        source2=$bin/$(grep "Mergelevels.R" $lib/CNA.config |cut -d "=" -f 2|xargs basename)
+        chrominfo=$lib/$(grep "chrominfo" $lib/CNA.config |cut -d "=" -f 2|xargs basename)
+        R CMD BATCH --vanilla --args -sample="$sample" -undoprune="$undo_prune" -alpha="$alpha" -chrominfo="$chrominfo" -makeFig="$makeFig" -source1="$source1" -source2="$source2" -outdir="$output" "$bin/CN_Seg_GC_filter_correctionRG.R" "$logs_folder/seg_log_file.log"
+}
+
+heatmap(){
+
+        source3=$(grep "heatmap.3.R" $lib/CNA.config |cut -d "=" -f 2)
+        R CMD BATCH --args -sample="$sample" -outdir="$output" -source3="$source3"  "$bin/heatmap.R" "$logs_folder/heatmap.log"
+}
+
+
+ main
+
+
+
+
